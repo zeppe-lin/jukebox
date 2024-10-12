a370c20de86b77eeeaee3db249e374849bddd73a
Alexandr Savca <alexandr.savca89@gmail.com>
2024-10-12 03:26:37 +0300

    add config.mk for future Makefile

---
+++ config.mk
@@ -0,0 +1,10 @@
+# project metadata
+NAME        = jukebox
+VERSION     = 1.1.15.3
+DIST        = ${NAME}-${VERSION}
+
+# paths
+PREFIX      = /usr
+MANPREFIX   = ${PREFIX}/share/man
+DATADIR     = ${PREFIX}/share
+BASHCOMPDIR = ${PREFIX}/share/bash-completion/completions
